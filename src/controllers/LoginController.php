ae8fd1f4d69033fa21cb2dbba79315f7b150064e
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-02-09 13:09:20 -0300

    preparando login 02

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -2,6 +2,7 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\handlers\LoginHandler;
 
 class LoginController extends Controller {
 
@@ -15,7 +16,24 @@ class LoginController extends Controller {
     }
 
     public function siginAction(){
-      echo 'Login recebido!';
+      //recebendo os dados do formulario
+      $email = filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL);
+      $password = filter_input(INPUT_POST,'password');
+
+      if($email && $password) {
+        //usando o handler "helper " login para verificar o login
+        $token = LoginHandler::verifyLogin($email,$password);
+        if($token){
+            $_SESSION['token'] = $token;
+            $this->redirect('/');
+        }else{
+          $_SESSION['flash'] = 'Email ou senha não conferem';
+          $this->redirect('/login');
+        }
+
+      }else {
+        $this->redirect('/login');
+      }
     }
 
     public function sigup() {
