f03e2e98d1ce6fc42f66a276ee6cad733eeaa6a5
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-02-18 16:37:27 -0300

    add script js e form no feed-editor

--- src/routes.php
+++ src/routes.php
@@ -11,6 +11,9 @@ $router->post('/login', 'LoginController@siginAction');
 $router->get('/cadastro', 'LoginController@sigup');
 $router->post('/cadastro', 'LoginController@sigupAction');
 
+//Feed editor
+$router->post('/post/new','PostController@new');
+
 //$router->get('/pesquisa');
 //$router->get('/perfil');
 //$router->get('/sair');
