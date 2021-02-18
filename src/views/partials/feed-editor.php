ac23991c660694875f92d134292b643081ff1497
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-02-18 16:07:32 -0300

    add base link no feed-editor

--- src/views/partials/feed-editor.php
+++ src/views/partials/feed-editor.php
@@ -2,12 +2,12 @@
   <div class="box-body">
       <div class="feed-new-editor m-10 row">
           <div class="feed-new-avatar">
-              <img src="media/avatars/<?=$user->avatar?>" />
+              <img src="<?=$base;?>/media/avatars/<?=$user->avatar?>" />
           </div>
           <div class="feed-new-input-placeholder">O que você está pensando, <?=$user->name;?></div>
           <div class="feed-new-input" contenteditable="true"></div>
           <div class="feed-new-send">
-              <img src="assets/images/send.png" />
+              <img src="<?=$base;?>/assets/images/send.png" />
           </div>
       </div>
   </div>
